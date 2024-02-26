d5e56baa6b1f5898ae93c5d6ddbd0bd56173c1ef
johnsamuel20 <joe.samuel.john.js@gmail.com>
2024-02-26 13:46:41 +0200

    ..

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <div className="app">
-      <Router>
+      <Router basename='/developer-portfolio'>
         <ScrollToTop/>
         <Switch>
           <Route path="/" exact component={Main} />

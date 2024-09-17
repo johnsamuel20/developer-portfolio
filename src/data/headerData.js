6d5af62bd005b551ba65eed8ad123b92032efdcb
johnsamuel20 <joe.samuel.john.js@gmail.com>
2024-09-17 15:38:29 +0300

    ..

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import johnImage from '../assets/images/porto.jpg'
 
 export const headerData = {
     name: 'John Samuel',
-    title: "Software Developer / Mechanical Engineer / Electric Guitarist ",
+    title: "Fullstack Developer / Mechanical Engineer / Electric Guitarist ",
     desciption:" Hey there, welcome to my portfolio! I'm a Software developer, Mechanical engineer, and Musician playing electric guitar, blending my passions into one exciting journey. With hands-on experience and a knack for creativity, I bring a unique perspective to every project I tackle. Explore my work and witness how I seamlessly weave my skills across these diverse fields. ",
     image: johnImage,
     resumePdf: resume
